8f7e3b43e615d45d8c7f3750a913a2b0ea40fb00
Mike Kennedy <mikennedy@gmail.com>
2017-01-22 21:51:36 -0800

    New theme

---
+++ master/analytics/analytics.js
@@ -0,0 +1,15 @@
+(function($) {
+
+	'use strict';
+
+	// Home Overview
+	$('#homeOverview a').on('mousedown', function() {
+		ga('send', 'event', 'overview', 'click', $(this).attr('href').replace(/.html/g, ""));
+	});
+
+	// Home Overview
+	$('#mainNav > li:first a').on('mousedown', function() {
+		ga('send', 'event', 'nav', 'click', $(this).attr('href').replace(/.html/g, ""));
+	});
+
+}).apply(this, [jQuery]);
